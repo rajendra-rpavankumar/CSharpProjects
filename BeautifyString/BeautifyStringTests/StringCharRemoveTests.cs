7a35d529cd7e1e3aab5c57f5567f019f7c1644f8
rajendra-rpavankumar <rajendra.rpavankumar@gmail.com>
2017-04-23 17:58:42 +0100

    Initial Commit - Remove Special Char with TDD

---
+++ BeautifyString/BeautifyStringTests/StringCharRemoveTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BeautifyString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifyString.Tests
+{
+    [TestClass()]
+    public class StringCharRemoveTests
+    {
+        [TestMethod()]
+        public void CleanstringTest()
+        {
+            //Assert.Fail();
+            StringCharRemove objRemoveSpecialChar = new StringCharRemove();
+            string actual=objRemoveSpecialChar.Cleanstring("ab!*#ca");
+            Assert.AreEqual("abca", actual);
+        }
+    }
+}
