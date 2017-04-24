f956a08932f2158721d2fec596a5c6ff4f1944f5
rajendra-rpavankumar <rajendra.rpavankumar@gmail.com>
2017-04-24 13:51:30 +0100

    Initial Commit - Remove White Space in String

---
+++ StringRemoveSpace/StringRemoveSpace/RemoveEmptySpace.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRemoveSpace
+{
+    public class RemoveEmptySpace
+    {
+        public string RemoveSpace(string input)
+        {
+            //StringBuilder sbRemoveSpace = new StringBuilder();
+            //for(int i=0;i<input.Length;i++)
+            //{
+            //    char c = input[i];
+            //    if(Char.IsWhiteSpace(input[i]))
+            //    {
+            //        continue;
+            //    }
+            //    sbRemoveSpace.Append(input[i]);
+            //}
+            //return sbRemoveSpace.ToString(); ;
+            var sb = new StringBuilder();
+            var prevIsWhitespace = false;
+            foreach (var ch in input)
+            {
+                var isWhitespace = char.IsWhiteSpace(ch);
+                if (prevIsWhitespace && isWhitespace)
+                {
+                    continue;
+                }
+                sb.Append(ch);
+                prevIsWhitespace = isWhitespace;
+            }
+            return sb.ToString();           
+        }
+    }
+}
---
+++ StringRemoveSpace/StringRemoveSpaceTests/RemoveEmptySpaceTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringRemoveSpace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRemoveSpace.Tests
+{
+    [TestClass()]
+    public class RemoveEmptySpaceTests
+    {
+        [TestMethod()]
+        public void RemoveSpaceTest()
+        {
+            //Assert.Fail();
+            RemoveEmptySpace objRemove = new RemoveEmptySpace();
+            string output = objRemove.RemoveSpace("Hello   World");
+            Assert.AreEqual("Hello World", output);
+        }
+    }
+}
